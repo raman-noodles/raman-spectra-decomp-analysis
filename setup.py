d3c73131ec31ce15f0b7beabd25dc2fedd47ce7a
Parker Steichen <parker7s@uw.edu>
2019-04-28 17:54:39 -0700

    old updates

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='raman-spectra-decomp-analysis',
       url='https://github.com/raman-noodles/raman-specrta-decomp-analysis',
       author='Raman Noodles Group, University of Washington (2019)',
       license='MIT',
-      packages=['raman-spectra-decomp-analysis'],
+      packages=['raman_spectra_decomp_analysis'],
       install_requires=['numpy', 'jcamp', 'requests', 'matplotlib', 'scipy', 'lmfit', 'peakutils', 'h5py', 'pandas', 'xlrd'])
