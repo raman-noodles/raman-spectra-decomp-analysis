c28260cd754c6a0a8b71b078734a1a7e5a46db05
Parker Steichen <parker7s@uw.edu>
2019-06-18 11:45:06 -0700

    updated tests and made PEP8 compliant

--- ramandecompy/tests/test_dataprep.py
+++ ramandecompy/tests/test_dataprep.py
@@ -7,17 +7,14 @@ from ramandecompy import dataprep
 def test_new_hdf5():
     """docstring"""
     dataprep.new_hdf5('function_test')
-    hdf5 = h5py.File('function_test.hdf5', 'r')
-    assert isinstance(hdf5, h5py._hl.files.File), 'output type is not a h5py._hl.files.File'
-    assert len(hdf5) == 0, 'output h5py._hl.files.File is not empty'
     # test inputs
     try:
         dataprep.new_hdf5(4.2)
     except TypeError:
         print('A float was passed to the function, and it was handled well with a TypeError.')
     os.remove('function_test.hdf5')
-        
-        
+
+
 def test_add_calibration():
     """docstring"""
     dataprep.new_hdf5('test')
@@ -35,10 +32,12 @@ def test_add_calibration():
     dataprep.add_calibration('test1.hdf5',
                              'ramandecompy/tests/test_files/Methane_Baseline_Calibration.xlsx')
     cal_file1 = h5py.File('test1.hdf5', 'r')
-    assert list(cal_file1.keys())[0] == 'Methane_Baseline_Calibration', 'filename label not applied correctly'
+    assert list(cal_file1.keys())[0] == 'Methane_Baseline_Calibration', """
+    filename label not applied correctly"""
     # test inputs
     try:
-        dataprep.add_calibration(4.2, 'ramandecompy/tests/test_files/CarbonMonoxide_Baseline_Calibration.xlsx')
+        dataprep.add_calibration(4.2, """ramandecompy/tests/test_files/
+        CarbonMonoxide_Baseline_Calibration.xlsx""")
     except TypeError:
         print('A float was passed to the function, and it was handled well with a TypeError.')
     try:
@@ -46,29 +45,32 @@ def test_add_calibration():
     except TypeError:
         print('A float was passed to the function, and it was handled well with a TypeError.')
     try:
-        dataprep.add_calibration('test.txt', 'ramandecompy/tests/test_files/CarbonMonoxide_Baseline_Calibration')
+        dataprep.add_calibration('test.txt', """ramandecompy/tests/
+        test_files/CarbonMonoxide_Baseline_Calibration""")
     except TypeError:
         print('A .txt file was passed to the function, and it was handled will with a TypeError.')
     os.remove('test.hdf5')
     os.remove('test1.hdf5')
-    
-    
+
+
 def test_add_experiment():
     """docstring"""
     dataprep.new_hdf5('exp_test')
-    dataprep.add_experiment('exp_test.hdf5', 'ramandecompy/tests/test_files/FA_3.6wt%_300C_25s.csv')
+    dataprep.add_experiment('exp_test.hdf5',
+                            'ramandecompy/tests/test_files/FA_3.6wt%_300C_25s.csv')
     exp_file = h5py.File('exp_test.hdf5', 'r')
     # test generated file
     assert len(exp_file) == 1, 'incorrect number of 1st order groups'
     assert list(exp_file.keys())[0] == '300C', '1st order group name incorrect'
-    assert len(exp_file['300C']) ==1, 'incorrect number of 2nd order groups'
+    assert len(exp_file['300C']) == 1, 'incorrect number of 2nd order groups'
     assert list(exp_file['300C'].keys())[0] == '25s', '2nd order group name incorrect'
     assert '300C/25s/wavenumber' in exp_file, 'x data (wavenumber) not stored correctly'
     assert '300C/25s/counts' in exp_file, 'y data (counts) not stored correctly'
     assert len(exp_file['300C/25s']) == 19, 'incorrect number of peaks + raw_data stored'
     # test inputs
     try:
-        dataprep.add_experiment(4.2, 'ramandecompy/tests/test_files/CarbonMonoxide_Baseline_Calibration.xlsx')
+        dataprep.add_experiment(4.2, """ramandecompy/tests/test_files/
+        CarbonMonoxide_Baseline_Calibration.xlsx""")
     except TypeError:
         print('A float was passed to the function, and it was handled well with a TypeError.')
     try:
@@ -76,9 +78,49 @@ def test_add_experiment():
     except TypeError:
         print('A float was passed to the function, and it was handled well with a TypeError.')
     try:
-        dataprep.add_experiment('exp_test.txt', 'ramandecompy/tests/test_files/CarbonMonoxide_Baseline_Calibration')
+        dataprep.add_experiment('exp_test.txt', """ramandecompy/tests/
+        test_files/CarbonMonoxide_Baseline_Calibration""")
     except TypeError:
         print('A .txt file was passed to the function, and it was handled will with a TypeError.')
+    os.remove('exp_test.hdf5')
+
+
+def test_adjust_peaks():
+    """
+    docsting
+    """
+    # generate test hdf5 file
+    dataprep.new_hdf5('exp_test')
+    dataprep.add_experiment('exp_test.hdf5', 'ramandecompy/tests/test_files/FA_3.6wt%_300C_25s.csv')
+    # peaks to add and drop form auto-fitting
+    add_list = [1270, 1350, 1385]
+    drop_list = ['Peak_01']
+    dataprep.adjust_peaks('exp_test.hdf5', '300C/25s', add_list, drop_list, plot_fits=True)
+    try:
+        dataprep.adjust_peaks(4.2, '300C/25s', add_list, drop_list, plot_fits=True)
+    except TypeError:
+        print('A float was passed to the function, and it was handled well with a TypeError.')
+    try:
+        dataprep.adjust_peaks('exp_test.txt', '300C/25s', add_list, drop_list, plot_fits=True)
+    except TypeError:
+        print('A .txt was passed to the function, and it was handled well with a TypeError.')
+    try:
+        dataprep.adjust_peaks('exp_test.hdf5', ['300C/25s', '400C/25s'],
+                              add_list, drop_list, plot_fits=True)
+    except TypeError:
+        print('A list was passed to the function, and it was handled well with a TypeError.')
+    try:
+        dataprep.adjust_peaks('exp_test.hdf5', '300C/25s', 'add_list', drop_list, plot_fits=True)
+    except TypeError:
+        print('A str was passed to the function, and it was handled well with a TypeError.')
+    try:
+        dataprep.adjust_peaks('exp_test.hdf5', '300C/25s', add_list, 'drop_list', plot_fits=True)
+    except TypeError:
+        print('A str was passed to the function, and it was handled well with a TypeError.')
+    try:
+        dataprep.adjust_peaks('exp_test.hdf5', '300C/25s', add_list, drop_list, plot_fits=3)
+    except TypeError:
+        print('An int was passed to the function, and it was handled well with a TypeError.')
     os.remove('exp_test.hdf5')
 
 
