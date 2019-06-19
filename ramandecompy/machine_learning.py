ca0be8904402c5c2224c103a7fb28b67e12e1b68
Parker Steichen <parker7s@uw.edu>
2019-06-19 15:21:09 -0700

    developing unit tests

--- ramandecompy/machine_learning.py
+++ ramandecompy/machine_learning.py
@@ -54,9 +54,95 @@ def keyfinder(hdf5_filename):
     return key_list
 
 
-def generate_spectra_dataset(hdf5_filename, target_compound, spectra_count):
+def interp_and_norm(hdf5_filename, compound):
+    """
+    This function interpolates and normalizes wavenumber and counts data for the specified
+    compound in an hdf5 calibration file.
+    
+    Args:
+        hdf5_filename (str): The name and location of the relevant hdf5 calibration datafile.
+        compound (str): The compound key in the hdf5 calibration datafile.
+
+    Returns:
+        tuple_list (list): A list of tuples containing pairs of wavenumber and counts values
+                           for the interpolated and normalized compound spectra.
+    """
+    # handling input errors
+    if not isinstance(hdf5_filename, str):
+        raise TypeError('Passed value of `hdf5_filename` is not a string! Instead, it is: '
+                        + str(type(hdf5_filename)))
+    if not hdf5_filename.split('/')[-1].split('.')[-1] == 'hdf5':
+        raise TypeError('`hdf5_filename` is not type = .hdf5! Instead, it is: '
+                        + hdf5_filename.split('/')[-1].split('.')[-1])
+    if not isinstance(compound, str):
+        raise TypeError('Passed value of `hdf5_filename` is not a string! Instead, it is: '
+                        + str(type(compound)))
+    # open hdf5_file
+    hdf5 = h5py.File(hdf5_filename, 'r')
+    # interpolate spectra
+    x_data = np.asarray(hdf5['{}/wavenumber'.format(compound)])
+    y_data = np.asarray(hdf5['{}/counts'.format(compound)])
+    interp_spectra = interpolate.interp1d(x_data, y_data, kind='cubic')
+    # get integer values across x_range, protecting against edge cases
+    x_range = np.arange(int(min(x_data)+1), int(max(x_data)))
+    y_interp = interp_spectra(x_range)
+    # normalize y_interp values
+    y_interp_norm = y_interp/max(y_interp)
+    # zip x and y values into tuples
+    tuple_list = list(zip(x_range, y_interp_norm))
+    # close hdf5 file
+    hdf5.close()
+    return tuple_list
+
+
+def apply_scaling(tuple_list, j, i ,target_index):
     """
     docstring
+    """
+    # unpack tuple_list
+    x_data, y_data = zip(*tuple_list)
+    y_data = np.asarray(y_data)
+    # alternate including target_compound or not
+    # if j is odd include target_compound
+    if j % 2 == 1:
+        if i == target_index:
+            y_data_scaled = y_data*np.random.uniform(0.1, 1)
+        else:
+            y_data_scaled = y_data*np.random.uniform()
+    # if j is even no target_compound
+    elif j % 2 == 0:
+        if i == target_index:
+            y_data_scaled = y_data*0
+        else:
+            y_data_scaled = y_data*np.random.uniform() 
+    else:
+        pass
+    # repack tuple_list
+    scaled_tuple_list = list(zip(x_data, y_data_scaled))
+    return scaled_tuple_list
+
+
+def generate_spectra_dataset(hdf5_filename, target_compound, spectra_count):
+    """
+    A function that generates a set of raman spectra which consist of a sum of normalized calibration
+    spectra. Each spectra contains all of the calibration spectra found in the hdf5 file specified
+    which are first interpolated and normalized, then scaled from 0.0 to 1.0 before being added together.
+    Every odd spectra will contain the target_compound at a 0.1 to 1.0 scaling while each even spectra
+    will not contain the target_compound.
+    
+    Args:
+        hdf5_filename (str): The name and location of the relevant hdf5 calibration datafile.
+        target_compound (str): The compound key in the hdf5 calibration datafile that will alternate
+                               as present and not present throughout the dataset.
+        spectra_count (int): An integer equal to the number of spectra desired for the dataset.
+        
+    Returns:
+        x_data (list): A list of the wavenumber values for the generated spectra. All elements of the
+                       list should be identical and are primarily used for identifying the wavenumber
+                       range of the dataset.
+        y_data (list): A list of the counts values for the generated spectra.
+        label (list): A list of the labels for each spectra indicating the presence or lack of the
+                      target_compound; (0 = no target, 1 = yes target).
     """
     hdf5 = h5py.File(hdf5_filename, 'r')
     # get list of compounds from hdf5 file
@@ -201,55 +287,6 @@ def combine_experiment(hdf5_filename, key, x_data, y_data, labels, num):
     return df
 
 
-def interp_and_norm(hdf5_filename, compound):
-    """
-    docstring
-    """
-    # open hdf5_file
-    hdf5 = h5py.File(hdf5_filename, 'r')
-    # interpolate spectra
-    x_data = np.asarray(hdf5['{}/wavenumber'.format(compound)])
-    y_data = np.asarray(hdf5['{}/counts'.format(compound)])
-    interp_spectra = interpolate.interp1d(x_data, y_data, kind='cubic')
-    # get integer values across x_range, protecting against edge cases
-    x_range = np.arange(int(min(x_data)+1), int(max(x_data)))
-    y_interp = interp_spectra(x_range)
-    # normalize y_interp values
-    y_interp_norm = y_interp/max(y_interp)
-    # zip x and y values into tuples
-    tuple_list = list(zip(x_range, y_interp_norm))
-    # close hdf5 file
-    hdf5.close()
-    return tuple_list
-
-
-def apply_scaling(tuple_list, j, i ,target_index):
-    """
-    docstring
-    """
-    # unpack tuple_list
-    x_data, y_data = zip(*tuple_list)
-    y_data = np.asarray(y_data)
-    # alternate including target_compound or not
-    # if j is odd include target_compound
-    if j % 2 == 1:
-        if i == target_index:
-            y_data_scaled = y_data*np.random.uniform(0.1, 1)
-        else:
-            y_data_scaled = y_data*np.random.uniform()
-    # if j is even no target_compound
-    elif j % 2 == 0:
-        if i == target_index:
-            y_data_scaled = y_data*0
-        else:
-            y_data_scaled = y_data*np.random.uniform() 
-    else:
-        pass
-    # repack tuple_list
-    scaled_tuple_list = list(zip(x_data, y_data_scaled))
-    return scaled_tuple_list
-
-
 def interpolated_spectra(hdf5_interpfilename, hdf5_calfilename, spectra_count):
     hdf5 = h5py.File(hdf5_calfilename, 'r+')
     # get list of compounds from hdf5 file
@@ -259,9 +296,11 @@ def interpolated_spectra(hdf5_interpfilename, hdf5_calfilename, spectra_count):
     compound_list = list(hdf5.keys())
     print(compound_list)
     for _, target_compound in enumerate(compound_list):
-        x_data, y_data, labels = machine_learning.generate_spectra_dataset(hdf5_calfilename, target_compound, spectra_count)
+        x_data, y_data, labels = machine_learning.generate_spectra_dataset(hdf5_calfilename,
+                                                                           target_compound, spectra_count)
         y_data_list.append(y_data)
         x_data_list.append(x_data)
         for i, label in enumerate(labels):
-            interpdf = machine_learning.combine_experiment(hdf5_interpfilename, 'interp_'+target_compound, x_data, y_data, label, i)      
+            interpdf = machine_learning.combine_experiment(hdf5_interpfilename, 'interp_'+target_compound,
+                                                           x_data, y_data, label, i)      
     return interpdf
