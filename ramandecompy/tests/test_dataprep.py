9f58df2bb17fa3f01996cb027fb1f785d52c2aa2
Parker Steichen <parker7s@uw.edu>
2019-06-20 08:33:31 -0700

    added docstrings where missing

--- ramandecompy/tests/test_dataprep.py
+++ ramandecompy/tests/test_dataprep.py
@@ -5,7 +5,9 @@ from ramandecompy import dataprep
 
 
 def test_new_hdf5():
-    """docstring"""
+    """
+    A function that tests that there are no errors in the `new_hdf5` function from dataprep.
+    """
     dataprep.new_hdf5('function_test')
     # test inputs
     try:
@@ -16,7 +18,13 @@ def test_new_hdf5():
 
 
 def test_add_calibration():
-    """docstring"""
+    """
+    A function that tests the `add_calibration` function from dataprep. It first tests that no
+    errors occur when the function is run before testing the output to ensure that the calibration
+    compound was sucessfully added and labeled appropriately. It checks that the proper number of
+    peaks were saved as well as the wavenumber, counts, and residuals. It tests both the custom and
+    automatic labeling functionality before finally ensuring that input errors are handled well.
+    """
     dataprep.new_hdf5('test')
     dataprep.add_calibration('test.hdf5',
                              'ramandecompy/tests/test_files/Methane_Baseline_Calibration.xlsx',
@@ -27,6 +35,7 @@ def test_add_calibration():
     assert len(cal_file['Methane']) == 4, 'more then 1 peak was stored'
     assert 'Methane/wavenumber' in cal_file, 'x data (wavenumber) not stored correctly'
     assert 'Methane/counts' in cal_file, 'y data (counts) not stored correctly'
+    assert 'Methane/residuals' in cal_file, 'residuals not stored correctly'
     # test that function assigns filename correctly as compound label
     dataprep.new_hdf5('test1')
     dataprep.add_calibration('test1.hdf5',
@@ -54,7 +63,13 @@ def test_add_calibration():
 
 
 def test_add_experiment():
-    """docstring"""
+    """
+    A function that tests the `add_experiment` function from dataprep. It first tests that no
+    errors occur when the function is run before testing the output to ensure that the
+    experimental data was sucessfully added and labeled appropriately. It checks that the
+    proper number of peaks were saved as well as the wavenumber, counts, and residuals.
+    Lastly it ensures that input errors are handled well.
+    """
     dataprep.new_hdf5('exp_test')
     dataprep.add_experiment('exp_test.hdf5',
                             'ramandecompy/tests/test_files/FA_3.6wt%_300C_25s.csv')
@@ -66,6 +81,7 @@ def test_add_experiment():
     assert list(exp_file['300C'].keys())[0] == '25s', '2nd order group name incorrect'
     assert '300C/25s/wavenumber' in exp_file, 'x data (wavenumber) not stored correctly'
     assert '300C/25s/counts' in exp_file, 'y data (counts) not stored correctly'
+    assert '300C/25s/residuals' in exp_file, 'residuals not stored correctly'
     assert len(exp_file['300C/25s']) == 19, 'incorrect number of peaks + raw_data stored'
     # test inputs
     try:
@@ -87,7 +103,9 @@ def test_add_experiment():
 
 def test_adjust_peaks():
     """
-    docsting
+    A function that tests the `adjust_peaks` function from dataprep. The function first looks to
+    see that no errors occur when running the function before then checking to ensure that input
+    errors are handled well.
     """
     # generate test hdf5 file
     dataprep.new_hdf5('exp_test')
@@ -125,7 +143,11 @@ def test_adjust_peaks():
 
 
 def test_view_hdf5():
-    """docstring"""
+    """
+    A function that tests the `view_hdf5` function from dataprep. The function first looks to
+    see that no errors occur when running the function before then checking to ensure that input
+    errors are handled well.
+    """
     # test inputs
     dataprep.view_hdf5('ramandecompy/tests/test_files/dataprep_experiment.hdf5')
     try:
