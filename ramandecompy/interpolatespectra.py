bef28c7ad174c26196e7a2b8f5958eb2cc1507c1
Parker Steichen <parker7s@uw.edu>
2019-06-24 19:59:34 -0700

    improved tests

--- ramandecompy/interpolatespectra.py
+++ ramandecompy/interpolatespectra.py
@@ -1,9 +1,11 @@
-"""This function takes in compounds calibration data files to generate interpolated data
+"""
+This function takes in compounds calibration data files to generate interpolated data
 made from randomized combinations of calibration peak data, and, using spectrafit,
 identifies peaks found in the fed-in known calibration peak spectra. It returns DataFrames
 with peak fitting descriptors to be used in either molar decomposition
 or machine learning applications.
  """
+
 import math
 import h5py
 import numpy as np
@@ -18,55 +20,139 @@ from ramandecompy import datavis
 from ramandecompy import dataimport
 
 
-def keyfinder(hdf5_filename):
+def interp_and_norm(hdf5_filename, compound):
     """
-    This function prints out a display of the list containing hdf5 key with
-    two layers. It is generally used with the large file of all the experimental
-    data from the dataimport function
-
+    This function interpolates and normalizes wavenumber and counts data for the specified
+    compound in an hdf5 calibration file.
+    
     Args:
-        hdf5_filename (str): the filename and location of an existing hdf5 file for inspection.
-
+        hdf5_filename (str): The name and location of the relevant hdf5 calibration datafile.
+        compound (str): The compound key in the hdf5 calibration datafile.
     Returns:
-        keys (list): list containing hdf5 key with two layers.
+        tuple_list (list): A list of tuples containing pairs of wavenumber and counts values
+                           for the interpolated and normalized compound spectra.
     """
+    # handling input errors
     if not isinstance(hdf5_filename, str):
-        raise TypeError("""Passed value of `hdf5_filename` is not a string!
-        Instead, it is: """+ str(type(hdf5_filename)))
+        raise TypeError('Passed value of `hdf5_filename` is not a string! Instead, it is: '
+                        + str(type(hdf5_filename)))
     if not hdf5_filename.split('/')[-1].split('.')[-1] == 'hdf5':
-        raise TypeError("""`hdf5_filename` is not type = .hdf5!
-        Instead, it is: """+ hdf5_filename.split('/')[-1].split('.')[-1])
-    keys = []
+        raise TypeError('`hdf5_filename` is not type = .hdf5! Instead, it is: '
+                        + hdf5_filename.split('/')[-1].split('.')[-1])
+    if not isinstance(compound, str):
+        raise TypeError('Passed value of `compound` is not a string! Instead, it is: '
+                        + str(type(compound)))
+    # open hdf5_file
     hdf5 = h5py.File(hdf5_filename, 'r')
-    for _, layer_1 in enumerate(list(hdf5.keys())):
-        if isinstance(hdf5[layer_1], h5py.Group):
-    #         print('\033[1m{}\033[0m'.format(layer_1))
-            for _, layer_2 in enumerate(list(hdf5[layer_1].keys())):
-                if isinstance(hdf5['{}/{}'.format(layer_1, layer_2)], h5py.Group):
-    #                 print('|    \033[1m{}\033[0m'.format(layer_2))
-                    keys.append('{}/{}'.format(layer_1, layer_2))
-                    for _, layer_3 in enumerate(list(hdf5['{}/{}'.format(layer_1,
-                                                                         layer_2)])):
-                        if isinstance(hdf5['{}/{}/{}'.format(layer_1, layer_2,
-                                                             layer_3)],
-                                      h5py.Group):
-    #                         print('|    |    \033[1m{}\033[0m/...'.format(layer_3))
-                            pass
-                        else:
-                            pass
-    #                         print('|    |    {}'.format(layer_3))
-                else:
-    #                 print('|    {}'.format(layer_2))
-                    keys.append('{}/{}'.format(layer_1, layer_2))
-        else:
-            pass
-    #         print('{}'.format(layer_1))
+    # interpolate spectra
+    x_data = np.asarray(hdf5['{}/wavenumber'.format(compound)])
+    y_data = np.asarray(hdf5['{}/counts'.format(compound)])
+    interp_spectra = interpolate.interp1d(x_data, y_data, kind='cubic')
+    # get integer values across x_range, protecting against edge cases
+    x_range = np.arange(int(min(x_data)+1), int(max(x_data)))
+    y_interp = interp_spectra(x_range)
+    # normalize y_interp values
+    y_interp_norm = y_interp/max(y_interp)
+    # zip x and y values into tuples
+    tuple_list = list(zip(x_range, y_interp_norm))
+    # close hdf5 file
     hdf5.close()
-    return keys
+    return tuple_list
+
+
+def apply_scaling(tuple_list, j, i ,target_index):
+    """
+    A function that applies a random scaling to a calibration spectra after
+    it has been interpolated and normalized. Every other `j` index will contain
+    the target compound corresponding to the 'target_index' in the list of 
+    calibration spectra.
+    
+    Args:
+        tuple_list (list): A list of x-y data tuples for the calibration spectra
+        j (int): This index is used to determine whether or not the target compound
+                 will be present in the spectra. If j is odd the the target compound
+                 will be scaled from 0.01 to 1. If j is even the target compound
+                 will be scaled to zero.
+        i (int): This index is used to determine which compound in the list the scaling
+                 should be applied to.
+        target_index (int): The index in the compound list that corresponds to the target
+    
+    Returns:
+        scaled_tuple_list (list). A list of randomly scaled x-y data tuples for the
+                                  specific the calibration spectra.
+    """
+    # handling input errors
+    if not isinstance(tuple_list, list):
+        raise TypeError('Passed value of `tuple_list` is not a list! Instead, it is: '
+                        + str(type(tuple_list)))
+    if not isinstance(tuple_list[0], tuple):
+        raise TypeError('Passed first value of `tuple_list` is not a tuple! Instead, it is: '
+                        + str(type(tuple_list[0])))
+    if not isinstance(j, int):
+        raise TypeError('Passed value of `j` is not an int! Instead, it is: '
+                        + str(type(j)))
+    if not isinstance(i, int):
+        raise TypeError('Passed value of `i` is not an int! Instead, it is: '
+                        + str(type(i)))
+    if not isinstance(target_index, int):
+        raise TypeError('Passed value of `target_index` is not an int! Instead, it is: '
+                        + str(type(target_index)))
+    # unpack tuple_list
+    x_data, y_data = zip(*tuple_list)
+    y_data = np.asarray(y_data)
+    # alternate including target_compound or not
+    # if j is odd include target_compound
+    if j % 2 == 1:
+        if i == target_index:
+            y_data_scaled = y_data*np.random.uniform(0.01, 1)
+        else:
+            y_data_scaled = y_data*np.random.uniform()
+    # if j is even no target_compound
+    elif j % 2 == 0:
+        if i == target_index:
+            y_data_scaled = y_data*0
+        else:
+            y_data_scaled = y_data*np.random.uniform() 
+    else:
+        pass
+    # repack tuple_list
+    scaled_tuple_list = list(zip(x_data, y_data_scaled))
+    return scaled_tuple_list
+
+
 def generate_spectra_dataset(hdf5_filename, target_compound, spectra_count):
     """
-    docstring
+    A function that generates a dataset of augmented spectra. It takes an hdf5_calibration file
+    and specifies a specific component that will be contained in 50% of spectra and omitted from
+    the remaining 50%. In this way machine learning methods can be used for component identification
+    when all possible components of a mixture are known and calibration files exist.
+    
+    Args:
+        hdf5_filename (str): The name and location of the relevant hdf5 calibration datafile.
+        target_compound (str): The compound key in the hdf5 calibration datafile.
+        spectra_count (int): The desired number of spectra to generate.
+
+    Returns:
+        x_data (list): A list containing identical x_data points for each spectra. Useful for plotting
+        y_data (list): A list containing the y_data points for each generated spectra.
+        labels (list): A list of 0s and 1s where 0 corresponds to no target_compound present and 1 
+                       indicating the target_compound is present in the compound. 
     """
+    # handling input errors
+    if not isinstance(hdf5_filename, str):
+        raise TypeError('Passed value of `hdf5_filename` is not a string! Instead, it is: '
+                        + str(type(hdf5_filename)))
+    if not hdf5_filename.split('/')[-1].split('.')[-1] == 'hdf5':
+        raise TypeError('`hdf5_filename` is not type = .hdf5! Instead, it is: '
+                        + hdf5_filename.split('/')[-1].split('.')[-1])
+    if not isinstance(target_compound, str):
+        raise TypeError('Passed value of `target_compound` is not a string! Instead, it is: '
+                        + str(type(target_compound)))
+    if not isinstance(spectra_count, int):
+        raise TypeError('Passed value of `spectra_count` is not an int! Instead, it is: '
+                        + str(type(spectra_count)))
+    if spectra_count <= 0:
+            raise ValueError('`spectra_count` must be an integer with a value greater than zero')
     hdf5 = h5py.File(hdf5_filename, 'r')
     # get list of compounds from hdf5 file
     compound_list = list(hdf5.keys())
@@ -113,6 +199,49 @@ def generate_spectra_dataset(hdf5_filename, target_compound, spectra_count):
         label.append(j % 2)
     hdf5.close()
     return x_data, y_data, label
+
+
+def keyfinder(hdf5_filename):
+    """
+    This function prints out a display of the list containing hdf5 key with
+    two layers. It is generally used with the large file of all the experimental
+    data from the dataimport function
+
+    Args:
+        hdf5_filename (str): the filename and location of an existing hdf5 file for inspection.
+
+    Returns:
+        keys (list): list containing hdf5 key with two layers.
+    """
+    if not isinstance(hdf5_filename, str):
+        raise TypeError("""Passed value of `hdf5_filename` is not a string!
+        Instead, it is: """+ str(type(hdf5_filename)))
+    if not hdf5_filename.split('/')[-1].split('.')[-1] == 'hdf5':
+        raise TypeError("""`hdf5_filename` is not type = .hdf5!
+        Instead, it is: """+ hdf5_filename.split('/')[-1].split('.')[-1])
+    keys = []
+    hdf5 = h5py.File(hdf5_filename, 'r')
+    for _, layer_1 in enumerate(list(hdf5.keys())):
+        if isinstance(hdf5[layer_1], h5py.Group):
+            for _, layer_2 in enumerate(list(hdf5[layer_1].keys())):
+                if isinstance(hdf5['{}/{}'.format(layer_1, layer_2)], h5py.Group):
+                    keys.append('{}/{}'.format(layer_1, layer_2))
+                    for _, layer_3 in enumerate(list(hdf5['{}/{}'.format(layer_1,
+                                                                         layer_2)])):
+                        if isinstance(hdf5['{}/{}/{}'.format(layer_1, layer_2,
+                                                             layer_3)],
+                                      h5py.Group):
+                            pass
+                        else:
+                            pass
+                else:
+                    keys.append('{}/{}'.format(layer_1, layer_2))
+        else:
+            pass
+    hdf5.close()
+    return keys
+
+
 def interpolatedfit(hdf5_filename, key, x_data, y_data, label, num):
     """
     This function adds interpolated Raman calibration data to an existing hdf5 file. It uses the
@@ -193,52 +322,7 @@ def interpolatedfit(hdf5_filename, key, x_data, y_data, label, num):
     exp_file.close()
     df = pd.DataFrame(data)
     return df
-def interp_and_norm(hdf5_filename, compound):
-    """
-    docstring
-    """
-    # open hdf5_file
-    hdf5 = h5py.File(hdf5_filename, 'r')
-    # interpolate spectra
-    x_data = np.asarray(hdf5['{}/wavenumber'.format(compound)])
-    y_data = np.asarray(hdf5['{}/counts'.format(compound)])
-    interp_spectra = interpolate.interp1d(x_data, y_data, kind='cubic')
-    # get integer values across x_range, protecting against edge cases
-    x_range = np.arange(int(min(x_data)+1), int(max(x_data)))
-    y_interp = interp_spectra(x_range)
-    # normalize y_interp values
-    y_interp_norm = y_interp/max(y_interp)
-    # zip x and y values into tuples
-    tuple_list = list(zip(x_range, y_interp_norm))
-    # close hdf5 file
-    hdf5.close()
-    return tuple_list
 
-def apply_scaling(tuple_list, j, i ,target_index):
-    """
-    docstring
-    """
-    # unpack tuple_list
-    x_data, y_data = zip(*tuple_list)
-    y_data = np.asarray(y_data)
-    # alternate including target_compound or not
-    # if j is odd include target_compound
-    if j % 2 == 1:
-        if i == target_index:
-            y_data_scaled = y_data*np.random.uniform(0.1, 1)
-        else:
-            y_data_scaled = y_data*np.random.uniform()
-    # if j is even no target_compound
-    elif j % 2 == 0:
-        if i == target_index:
-            y_data_scaled = y_data*0
-        else:
-            y_data_scaled = y_data*np.random.uniform() 
-    else:
-        pass
-    # repack tuple_list
-    scaled_tuple_list = list(zip(x_data, y_data_scaled))
-    return scaled_tuple_list
 
 def combined_interpolatedfit(hdf5_interpfilename, hdf5_calfilename, spectra_count):
     """
